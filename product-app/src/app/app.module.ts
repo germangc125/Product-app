fb1cf16d2e8fcdecb66b5912eb5e56a27e6ffa38
German Gracia <germangc125@hotmail.com>
2016-12-03 11:15:08 -0500

    Version 1

--- product-app/src/app/app.module.ts
+++ product-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { ProductPage } from '../pages/product/product';
+import { ProfilePage } from '../pages/profile/profile';
+import { LogoutPage } from '../pages/Logout/Logout';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    ProductPage,
+    ProfilePage,
+    LogoutPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +18,9 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    ProductPage,
+    ProfilePage,
+    LogoutPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
