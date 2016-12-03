de688c3c38e0509e3b3015f4861756234f5aa3ef
German Gracia <germangc125@hotmail.com>
2016-12-03 11:15:21 -0500

    version1

--- product-app/src/app/app.component.ts
+++ product-app/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { ProductPage } from '../pages/product/product';
+import { ProfilePage } from '../pages/profile/profile';
+import { LogoutPage } from '../pages/Logout/Logout';
 
 
 @Component({
@@ -12,7 +13,7 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = ProductPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -21,8 +22,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Home', component: ProductPage },
+      { title: 'My account', component: ProfilePage },
+      { title: 'Log-out', component: LogoutPage }
     ];
 
   }
